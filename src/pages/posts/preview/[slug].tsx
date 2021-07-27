6a96cfba3888485feedd83c4710b5472e88e5da4
Henrique França <euhenriquefranca@gmail.com>
2021-07-27 20:05:47 -0300

    redirect

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import {  GetStaticProps } from "next";
+import { GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -7,7 +7,7 @@ import { RichText } from "prismic-dom";
 import { useEffect } from "react";
 import { getPrismicClient } from "../../../services/prismic";
 
-import styles from "../post.module.scss"
+import styles from "../post.module.scss";
 
 interface PostPreviewProps {
   post: {
@@ -15,18 +15,17 @@ interface PostPreviewProps {
     title: string;
     content: string;
     updatedAt: string;
-  }
+  };
 }
 
-export default function PostPreview({ post }: PostPreviewProps ) {
-  const [session] = useSession()
+export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
 
   useEffect(() => {
-    if(session?.activeSubscription) {
-      router.push(`/posts/${post.slug}`)
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
     }
-
-  }, [session])
+  }, [session]);
 
   return (
     <>
@@ -40,7 +39,8 @@ export default function PostPreview({ post }: PostPreviewProps ) {
           <time>{post.updatedAt}</time>
           <div
             className={`${styles.postContent} ${styles.previewContent}`}
-            dangerouslySetInnerHTML={{__html: post.content}} />
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
 
           <div className={styles.continueReading}>
             Wanna continue reading?
@@ -51,36 +51,39 @@ export default function PostPreview({ post }: PostPreviewProps ) {
         </article>
       </main>
     </>
-  )
+  );
 }
 
 export const getStaticPaths = () => {
   return {
     paths: [],
-    fallback: 'blocking'
-  }
-}
+    fallback: "blocking",
+  };
+};
 
-export const getStaticProps: GetStaticProps = async ({  params }) => {
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
 
-  const prismic = getPrismicClient()
+  const prismic = getPrismicClient();
 
-  const response = await prismic.getByUID('post', String(slug), {})
+  const response = await prismic.getByUID("post", String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content.splice(0, 3)),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    })
-  }
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
 
   return {
-    props: { post }
-  }
+    props: { post },
+    redirect: 60 * 60 * 30, // 30 minutes
+  };
 };

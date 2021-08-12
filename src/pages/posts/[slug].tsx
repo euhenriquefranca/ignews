6407ab8f6a4a123efa90b8d5f4a277b56dedd920
Henrique França <euhenriquefranca@gmail.com>
2021-08-12 09:35:29 -0300

    new deploy

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 
-import styles from "./post.module.scss"
+import styles from "./post.module.scss";
 
 interface PostProps {
   post: {
@@ -12,10 +12,10 @@ interface PostProps {
     title: string;
     content: string;
     updatedAt: string;
-  }
+  };
 }
 
-export default function Post({ post }: PostProps ) {
+export default function Post({ post }: PostProps) {
   return (
     <>
       <Head>
@@ -28,42 +28,49 @@ export default function Post({ post }: PostProps ) {
           <time>{post.updatedAt}</time>
           <div
             className={styles.postContent}
-            dangerouslySetInnerHTML={{__html: post.content}} />
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
         </article>
       </main>
     </>
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
   const session = await getSession({ req });
   const { slug } = params;
 
-  if(!session?.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: '/',
-        permanent: false
-      }
-    }
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
 
-  const prismic = getPrismicClient(req)
+  const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID('post', String(slug), {})
+  const response = await prismic.getByUID("post", String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    })
-  }
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
 
   return {
-    props: { post }
-  }
+    props: { post },
+  };
 };
